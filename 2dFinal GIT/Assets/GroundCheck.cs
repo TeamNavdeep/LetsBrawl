fbfeb07d6c54af8cd33edea3b9ef6bfb174441a2
Justin Caguiat <justin.caguiat@gmail.com>
2015-01-16 17:51:36 -0500

    First Upload
    PlayerController stuff

---
+++ 2dFinal GIT/Assets/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Platform")
+		{
+			PlayerController.isGrounded = true;
+			PlayerController.doubleJump = false;
+			Debug.Log("GROUND");
+		}
+
+	}
+}
---
+++ 2dFinal GIT/Assets/MoveRight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class MoveRight : MonoBehaviour {
+	Touch touch;
+	public GameObject cube;
+	private Vector3 movement = Vector3.left * 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase != TouchPhase.Ended && Input.GetTouch (0).phase != TouchPhase.Canceled) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay( Input.GetTouch(0).position );
+			RaycastHit hit;
+
+			if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == cube)
+			{
+				Debug.Log("CLICK RIGHT");
+				movement = Vector3.right * 0.1f;
+//				if(Input.GetTouch(0).phase != TouchPhase.Ended && Input.GetTouch(0).phase != TouchPhase.Canceled)
+					cube.transform.Translate(movement);
+			}
+		}
+
+	}
+	
+}

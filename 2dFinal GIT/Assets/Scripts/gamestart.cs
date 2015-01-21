18ca0d0ef9799a990270a2e8c9640bb98c73a5a8
Navdeep Hayer <Navdeephayer@gmial.com>
2015-01-21 15:41:32 -0500

    Uploading Gamestart and gamestate Scripts

---
+++ 2dFinal GIT/Assets/Scripts/gamestart.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class gamestart : MonoBehaviour 
+{		
+	// Our Startscreen GUI
+
+	void OnGUI () 
+	{
+		if(GUI.Button(new Rect (Screen.width/2 - 75, Screen.height/2, 150, 30), "Play Game"))
+		{
+			startGame();
+		}	
+		if (GUI.Button (new Rect (Screen.width/2 - 75, Screen.height/2 + 35, 150, 30), "Customize Character"))
+		{
+			print("Customize Character Meun");
+		}
+		if (GUI.Button (new Rect (Screen.width/2 - 75, Screen.height/2 + 70, 150, 30), "Achievements"))
+		{
+			print("Achievements");
+		}
+	}
+	
+	private void startGame()
+	{
+		print("Starting game");
+		
+		DontDestroyOnLoad(gamestate.Instance);
+		gamestate.Instance.startState();
+	}
+}
---
+++ 2dFinal GIT/Assets/Scripts/gamestate.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gamestate : MonoBehaviour {
+
+
+	// Declare properties
+	private static gamestate instance;
+	private string activeLevel;			
+	private string name;					
+	private int maxHP;					
+	private int maxMP;				
+	private int hp;					
+	private int mp;						
+	private int str;						
+	private int vit;							
+	private int dex;						
+	private int exp;
+
+	// ---------------------------------------------------------------------------------------------------
+	// gamestate()
+	// --------------------------------------------------------------------------------------------------- 
+	// Creates an instance of gamestate as a gameobject if an instance does not exist
+	// ---------------------------------------------------------------------------------------------------
+	public static gamestate Instance
+	{
+		get
+		{
+			if(instance == null)
+			{
+				instance =  new GameObject("gamestate").AddComponent<gamestate>();
+				//instance = new GameObject("gamestate").AddComponent();
+			}
+			
+			return instance;
+		}
+	}	
+	
+	// Sets the instance to null when the application quits
+	public void OnApplicationQuit()
+	{
+		instance = null;
+	}
+	// startState()
+	// --------------------------------------------------------------------------------------------------- 
+	// Creates a new game state
+	// ---------------------------------------------------------------------------------------------------
+	public void startState()
+	{
+		print ("Creating a new game state");
+
+		// Set default properties:
+		activeLevel = "Level 1";
+		name = "My Character";
+		maxHP = 250;
+		maxMP = 60;
+		hp = maxHP;
+		mp = maxMP;
+		str = 6;
+		vit = 5;
+		dex = 7;
+		exp = 0;
+
+		//Application.LoadLevel("level1");
+	}
+}

fd8ceac7571cd2b6c48d973f9dcb6d146e8d984f
Louis Smith <someonewholives@hotmail.com>
2015-02-09 23:42:59 -0500

    Added Multiplayer Capabilities
    I added what I worked on today during our meeting. I still need to get
    the animations working.

---
+++ 2dFinal GIT/Assets/Scripts/Main/NetworkScript.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkScript : MonoBehaviour {
+
+	//Global Objects/Variables
+	public GUISkin skinGUI;
+	public string gameName = "Let's Brawl ver 0.4";//Name of the Game
+	public string serverName = "Game Match";//Custom name of the server
+	public string hostName = "Louis";//Host Name
+	private string password = "";
+	private bool wantPass = false;
+	private string errorMessage = "";
+
+	private bool refreshing = true;
+	private HostData[] hData = new HostData[6];
+	private string waitMsg = "";
+
+	public GameObject player;
+	public GameObject startPos;
+	
+	void startServer(){
+		if (serverName == null || serverName == "")
+			errorMessage = "Enter a name in the Server Name field.";
+		else if (wantPass && password == "")
+			errorMessage = "Enter a Password";
+		else{
+			if (wantPass)
+				Network.incomingPassword = password;
+			Network.InitializeServer (3, 25001, !Network.HavePublicAddress());//Number of players, port number, makes sure the port isn't taken
+			MasterServer.RegisterHost (gameName, serverName, hostName);
+			Debug.Log("Starting Server");
+		}
+	}
+	
+	void refreshHostList(){
+		MasterServer.RequestHostList (gameName);
+		refreshing = true;
+		for (int i = 0; i < hData.Length; i++){
+			hData[i] = null;
+		}
+	}
+
+	void createPlayer(){
+		if (Network.isServer){
+			Network.Instantiate (player, startPos.transform.position, player.transform.rotation, 0);
+		}
+		else if (Network.isClient)
+			Network.Instantiate (player, startPos.transform.position, player.transform.rotation, 1);
+	}
+
+	void Update () {
+		if (refreshing){
+			if (MasterServer.PollHostList().Length > 0){
+				refreshing = false;
+				Debug.Log(MasterServer.PollHostList ().Length);
+				hData = MasterServer.PollHostList ();
+				waitMsg = "";
+			}
+			else{
+				waitMsg = "Finding Games...";
+			}
+		}
+		else{
+			if (MasterServer.PollHostList().Length <= 0){
+				waitMsg = "Can't Find Games";
+			}
+		}
+	}
+
+	void OnServerInitialized(){
+		Debug.Log ("Server Initialized");
+		createPlayer ();
+	}
+
+	void OnConnectedToServer(){
+		createPlayer ();
+	}
+
+	void OnDisconnectedFromServer(){
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		for (int i = 0; i < players.Length; i++){
+			Destroy(players[i]);
+		}
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent mse){
+		if(mse == MasterServerEvent.RegistrationSucceeded)
+			Debug.Log("Registered Server!");
+		else{
+			errorMessage = "Server already taken.";
+			Debug.Log("Registration Failed");
+		}
+	}
+	
+	void OnGUI(){
+		Rect hg1 = new Rect (0, 0, Screen.width / 2, Screen.height);
+		Rect info = new Rect (hg1.width / 2 - ((hg1.width - 50) / 2), 100, hg1.width - 100, hg1.height - 400);
+		
+		GUI.skin = skinGUI;
+		if (!Network.isServer && !Network.isClient){
+			GUI.BeginGroup (hg1);//Gives the GUI a default size
+			GUI.Box (new Rect(0, 0, hg1.width, hg1.height), "Host Game");
+			GUI.BeginGroup (info);
+			GUI.Label (new Rect (20, 20, 80, 20), "Server Name:");
+			serverName = GUI.TextField (new Rect(100, 20, 200, 20), serverName, 25);
+			
+			wantPass = GUI.Toggle (new Rect (20, 50, 80, 20), wantPass, "Password");
+			
+			if (wantPass){
+				GUI.Label(new Rect(20, 70, 100, 20), "Enter Password:");
+				password = GUI.PasswordField(new Rect(120, 70, 150, 20), password, '#', 15);
+			}
+			GUI.Label (new Rect (20, info.height - 80, info.width - 40, 20), errorMessage);
+			
+			Rect btnSize1 = new Rect (info.width / 2 - 50, info.height - 50, 100, 50);
+			GUI.EndGroup ();
+			
+			if (GUI.Button(new Rect(hg1.width / 2 - 100, hg1.height - 150, 200, 100), "Start Server")){
+				startServer();
+			}
+			
+			GUI.EndGroup ();
+		}
+		else{
+			
+		}
+
+		Rect hg2 = new Rect (Screen.width / 2, 0, Screen.width / 2, Screen.height);
+
+		if (!Network.isServer && !Network.isClient){
+			GUI.BeginGroup (hg2);
+			GUI.Box (new Rect (0, 0, hg2.width, hg2.height), "Join Game");
+			
+			if (GUI.Button(new Rect(20, 20, 80, 40), "Refresh")){
+				refreshHostList();
+				Debug.Log("Refresh");
+			}
+			GUI.Label (new Rect (hg2.width / 2 - 50, hg2.height / 2 - 10, 100, 20), waitMsg);
+			Rect gameBox;
+			if (!refreshing){
+				for (int i = 0; i < hData.Length; i++){
+					if (hData[i] != null){
+						gameBox = new Rect (hg2.width / 2 - ((hg2.width - 200) / 2), 60 + (60 * i), hg2.width - 200, 60);
+						GUI.BeginGroup(gameBox);
+						GUI.Box(gameBox, "" + (1 + 1));
+						GUI.Label(new Rect(gameBox.width / 2 - 100, gameBox.height / 2 - 10, 200, 20), hData[i].gameName);
+						GUI.Label(new Rect(20, 10, 200, 20), "Host: " + hData[i].comment);
+						GUI.Label(new Rect(20, gameBox.height - 30, 50, 20), hData[i].connectedPlayers + "/" + hData[i].playerLimit);
+						if (GUI.Button(new Rect(gameBox.width - 100, 0, 40, 40), "Join")){
+							if (hData[i].passwordProtected){
+								
+							}
+							else{
+								Network.Connect(hData[i]);
+							}
+						}
+						GUI.EndGroup();
+					}
+				}
+			}
+			
+			if (GUI.Button(new Rect(hg2.width / 2 - 100, hg2.height - 150, 200, 100), "Menu")){
+				Application.LoadLevel("GameSelection");
+			}
+			
+			GUI.EndGroup ();
+		}
+		if (Network.isServer){
+			if (GUI.Button(new Rect(20, 20, 200, 100), "Close Server")){
+				Network.Disconnect();
+			}
+		}
+		if (Network.isClient){
+			if (GUI.Button(new Rect(20, 20, 200, 100), "Disconnect")){
+				Network.Disconnect();
+			}
+		}
+	}
+}
--- 2dFinal GIT/Assets/Scripts/Main/PlayerController.cs
+++ 2dFinal GIT/Assets/Scripts/Main/PlayerController.cs
@@ -21,10 +21,15 @@ public class PlayerController : MonoBehaviour {
 	private int fingertouch= 0;
 	//animation
 	private Animator anim;
+	private NetworkView netView;
 
 	void Start () 
 	{
 		anim = this.GetComponent<Animator> ();
+		netView = this.GetComponent<NetworkView> ();
+		leftBtn = GameObject.Find ("Left_Btn");
+		rightBtn = GameObject.Find ("Right_Btn");
+		jumpBtn = GameObject.Find ("Jump_Btn");
 	}
 	
 	void Update () 
@@ -36,49 +41,51 @@ public class PlayerController : MonoBehaviour {
 		//Input.GetTouch (0).phase != TouchPhase.Ended && Input.GetTouch (0).phase != TouchPhase.Canceled
 		// to check if ray cast is hitting an object via touch
 		// if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == GAMEOBJECT)
-		if (Input.touchCount > 0 ) 
-		{
-			foreach(Touch t in Input.touches)
+		if (netView.isMine){
+			if (Input.touchCount > 0 ) 
 			{
-				Ray ray = Camera.main.ScreenPointToRay( t.position );
-				RaycastHit hit;
-				if(t.phase != TouchPhase.Ended && t.phase != TouchPhase.Canceled)
+				foreach(Touch t in Input.touches)
 				{
-					if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == leftBtn)
+					Ray ray = Camera.main.ScreenPointToRay( t.position );
+					RaycastHit hit;
+					if(t.phase != TouchPhase.Ended && t.phase != TouchPhase.Canceled)
 					{
-						Debug.Log("CLICK LEFT");
-						anim.SetBool("Moving", true);
-						player.transform.rotation = Quaternion.Euler(0.0f, 270.0f, 0.0f);
-						player.transform.Translate(movement);
-
+						if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == leftBtn)
+						{
+							Debug.Log("CLICK LEFT");
+							anim.SetBool("Moving", true);
+							player.transform.rotation = Quaternion.Euler(0.0f, 270.0f, 0.0f);
+							player.transform.Translate(movement);
+							
+						}
+						else if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == rightBtn)
+						{
+							Debug.Log("CLICK RIGHT");
+							anim.SetBool("Moving", true);
+							player.transform.rotation = Quaternion.Euler(0.0f, -270.0f, 0.0f);
+							player.transform.Translate(movement);
+						}
 					}
-					else if ( Physics.Raycast(ray, out hit) && hit.transform.gameObject == rightBtn)
+					else
 					{
-						Debug.Log("CLICK RIGHT");
-						anim.SetBool("Moving", true);
-						player.transform.rotation = Quaternion.Euler(0.0f, -270.0f, 0.0f);
-						player.transform.Translate(movement);
+						anim.SetBool("Moving", false);
 					}
-				}
-				else
-				{
-					anim.SetBool("Moving", false);
-				}
-				if(t.phase == TouchPhase.Began)
-				{
-					if (Physics.Raycast(ray, out hit) && hit.transform.gameObject == jumpBtn && isGrounded)
+					if(t.phase == TouchPhase.Began)
 					{
-						anim.SetTrigger("Jump");
-						isGrounded = false;
-						Debug.Log("CLICK JUMP");
-						player.rigidbody.AddForce(new Vector2(0,1000f));
-
-					}
-					else if(Physics.Raycast(ray, out hit) && hit.transform.gameObject == jumpBtn && !isGrounded && doubleJump)
-					{
-						anim.SetTrigger("Double Jump");
-						player.rigidbody.AddForce(new Vector2(0,750f));
-						doubleJump=false;
+						if (Physics.Raycast(ray, out hit) && hit.transform.gameObject == jumpBtn && isGrounded)
+						{
+							anim.SetTrigger("Jump");
+							isGrounded = false;
+							Debug.Log("CLICK JUMP");
+							player.rigidbody.AddForce(new Vector2(0,1000f));
+							
+						}
+						else if(Physics.Raycast(ray, out hit) && hit.transform.gameObject == jumpBtn && !isGrounded && doubleJump)
+						{
+							anim.SetTrigger("Double Jump");
+							player.rigidbody.AddForce(new Vector2(0,750f));
+							doubleJump=false;
+						}
 					}
 				}
 			}

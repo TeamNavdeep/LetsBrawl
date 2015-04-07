65ce38c8f9520d41d865a59255dad2e5ebb4fac4
alexgomes09 <alex_09hg@yahoo.com>
2015-04-06 23:36:31 -0400

    Some progress on Customize character menu | Still working

--- 2dFinal GIT/Assets/Scripts/Main/gamestart.cs
+++ 2dFinal GIT/Assets/Scripts/Main/gamestart.cs
@@ -5,6 +5,18 @@ using System.Collections;
 public class gamestart : MonoBehaviour 
 {		
 	// Our Startscreen GUI
+	GameObject customizeMenu,GUIButton;
+
+	void Start(){
+		customizeMenu = GameObject.Find("CustomizeMenu");
+		GUIButton = GameObject.Find("GUI");
+		customizeMenu.SetActive(false);
+	}
+
+	public void BackButton(){
+		customizeMenu.SetActive(false);
+		GUIButton.SetActive(true);
+	}
 
 	void OnGUI () 
 	{
@@ -14,7 +26,8 @@ public class gamestart : MonoBehaviour
 		}	
 		if (GUI.Button (new Rect (Screen.width/2 - 75, Screen.height/2 + 35, 150, 30), "Customize Character"))
 		{
-			CustomizeCharacter();
+			customizeMenu.SetActive(true);
+			GUIButton.SetActive(false);
 		}
 		if (GUI.Button (new Rect (Screen.width/2 - 75, Screen.height/2 + 70, 150, 30), "Achievements"))
 		{
@@ -26,13 +39,6 @@ public class gamestart : MonoBehaviour
 	{
 		//Launch Louis Script
 		print("Starting game scene");
-		
-		DontDestroyOnLoad(gamestate.Instance);
-		gamestate.Instance.startState();
-	}
-	private void CustomizeCharacter()
-	{
-		print("Customize Character scene");
 		
 		DontDestroyOnLoad(gamestate.Instance);
 		gamestate.Instance.startState();
